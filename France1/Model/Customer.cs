915fdf2b4d8bc9a3328670f1906179988316d303
ProjectRKA <projectrka@gmail.com>
2015-12-07 09:17:14 +0100

    Added Customer and CustomerList classes to the model.

---
+++ France1/Model/Customer.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace France1.Model
+{
+    class Customer
+    {
+        private string _x;
+        private string _y;
+        private string _password;
+        private string _username;
+
+        public Customer(string x, string y)
+        {
+            this._x = x;
+            this._y = y;
+        }
+
+        public Customer(string password, string, username)
+        {
+            this._password = password;
+            this._username = username;
+        }
+        public void Login()
+        {
+
+        }
+    }
+}
---
+++ France1/Model/CustomerList.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace France1.Model
+{
+    class CustomerList
+    {
+        private string x; //username
+        private string y; //password
+
+        private List<Customer> list;
+
+        public string Username
+        {
+            get { return x; }
+            set { x = value; }
+        }
+        public string Password
+        {
+            get { return y; }
+            set { y = value; }
+        }
+
+        public CustomerList()
+        {
+            list = new List<Customer>();
+        }
+
+        public void Register()
+        {
+            list.Add(new Customer(x, y));
+            x = null;
+            y = null;
+        }
+    }
+    
+}
